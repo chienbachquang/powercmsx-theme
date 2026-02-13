5b0a4e5bc6c69c494d218f11d7367692787dcd65
Quang Chien <chien_bach_quang@bita.jp>
2026-02-13 15:23:12 +0700

    chore: remove various unused static assets.

--- static/assets/common.ts
+++
@@ -1,8 +0,0 @@
-import { Collapse } from './components/collapse/_collapse';
-
-(function (global) {
-  // Global component IIFE
-  global.Component = global.Component || {};
-
-  global.Component.Collapse = Collapse;
-})(window);
